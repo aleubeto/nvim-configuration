41b36cf3ab1457a59f5648621e2643742890d5fc
aubeto <aleubeto.dev@gmail.com>
2024-05-19 21:00:45 -0600

    Created repository.

---
+++ lua/plugins/init.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier",
+        "eslint-lsp",
+        -- Node and next.js
+        "typescript-language-server",
+        "tailwindcss-language-server",
+        -- Python
+        "pyright", "black",
+  		},
+  	},
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "html", "css",
+        "javascript", "typescript", "tsx",
+        "python", "rust", "go", "java",
+  		},
+  	},
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require("configs.null-ls")
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "html",
+    },
+    config = function ()
+      require("nvim-ts-autotag").setup()
+    end
+  }
+}
