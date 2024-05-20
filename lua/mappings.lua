acc1912ec48fc2e943abf8f3ad23ae7f58518876
aubeto <aleubeto.dev@gmail.com>
2024-05-20 11:45:19 -0600

    feat(mappings): Create shortcut to exit the editor. Created the mapping <leader>q to close every open buffer and exit neovim.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,3 +7,4 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map('n', '<leader>s', ':SaveWithoutFormat<CR>', { desc = "Save without formatting" })
+map('n', '<leader>q', ':qa<CR>', { desc = "Exit editor" })
